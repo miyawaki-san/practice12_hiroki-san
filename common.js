add61f89b369d05770087cdf69abf6b5ca8a6cf9
miyawaki <hesogoma@gmail.com>
2021-04-28 21:39:54 +0900

    [rev]0428-1

--- common.js
+++ common.js
@@ -31,7 +31,21 @@ $('.burgerBtnArea').on('click',function(){
   $('#header').toggleClass('open');
 });
 
-$('.overlay').on('click', function() {
+$('.headerMenu__item').on('click',function(){
   $('#header').toggleClass('open');
 });
 
+// $('.overlay').on('click', function() {
+//   $('#header').toggleClass('open');
+// });
+
+$(function(){
+  $('a[href^="#"]').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
